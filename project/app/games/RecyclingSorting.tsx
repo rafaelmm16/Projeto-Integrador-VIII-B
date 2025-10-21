1112699e73713b2a8acc4b380117d8aa26f014e8
rafaelmm16 <rafael.merlomendes@gmail.com>
2025-10-21 10:49:23 -0300

    fix: corrigir erro de digitação na atualização do nome do jogador

--- project/app/games/RecyclingSorting.tsx
+++ project/app/games/RecyclingSorting.tsx
@@ -222,7 +222,7 @@ export default function RecyclingSorting() {
                   <input
                     type="text"
                     value={playerName}
-                    onChange={(e) => setPlayerName(e.targe.value)}
+                    onChange={(e) => setPlayerName(e.target.value)}
                     placeholder="Digite seu nome"
                     className="flex-1 px-4 py-2 border-2 border-gray-300 rounded-lg focus:border-blue-500 focus:outline-none"
                   />
